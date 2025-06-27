2de39c7e2a1584d6bb8aa9f0770e9ce2ce21aa54
jac0230023 <jac0230023@aluno.firjansenaisesi.com.br>
2025-06-27 20:10:15 +0000

    Comentarios e complemento do cod

--- NotasALuno/src/Main.java
+++ NotasALuno/src/Main.java
@@ -3,32 +3,51 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double mediaMelhorAluno = -1, mediaPiorAluno = 11, somaMedia = 0, nota = 0;
+        double mediaMelhorAluno = -1, mediaPiorAluno = 11;
         String nomeMelhorAluno = "", nomePiorAluno = "";
 
-        for (int qtdAluno = 0; qtdAluno < 10; qtdAluno++) {
+        for (int qtdAluno = 0; qtdAluno < 3; qtdAluno++) { //intera pedido de nome de aluno
+            
 
-
-            System.out.printf("Nome do %d° Aluno: ", qtdAluno + 1);
-            String nomeAluno = input.nextLine();
-
-            for (int i = 0; i < 4; i++) {
-                System.out.printf("%d° Nota: ", i + 1);
-                nota = input.nextDouble();
-                while (nota < 0 || nota > 10) {
-                    System.out.println("Nota Invalida!\n");
-                    System.out.printf("%d° Nota: ", i + 1);
-                    nota = input.nextDouble();
+            System.out.printf("Nome do %d° Aluno: ", qtdAluno + 1); //imprime pedido nome do aluno
+            String nomeAluno = input.nextLine(); //recebe nome aluno
+            double somaMedia = 0;
+            for (int i = 0; i < 4; i++) { // intera pedido de 4 notas para cada aluno
+                
+                double nota = 0;
+                System.out.printf("%d° Nota: ", i + 1);//imprimi pedido de nota
+                nota = input.nextDouble();//recebe nota
+                while (nota < 0 || nota > 10) { //enquanto entrada do usuario nao estiver entre 0 e 10 incluso vai solicitar uma nova nota
+                    System.out.println("Nota Invalida!\n"); // informa que valor anteriro foi invalido    
+                    System.out.printf("%d° Nota: ", i + 1); //imprimi pedido de nota
+                    nota = input.nextDouble(); //recebe nota
                 }
-                somaMedia += nota;
+                input.nextLine(); //consome buff
+                somaMedia += nota;    
             }
-            double media = somaMedia / 4;
+            double media = somaMedia / 4; //calcula media e armazena na variavel
 
-            if (media > 0 && media < 5) {
-                System.out.printf("Aluno %f Aprovado com %d", nomeAluno, media);
+            //informa qual a situação do aluno
+            if (media > 0 && media < 6) { 
+                System.out.printf("Aluno %s Reprovado com %.2f.\n", nomeAluno, media);
+            }else if(media >= 6 && media< 8){
+                System.out.printf("Aluno %s em RECUPERAÇÃO com %.2f.\n", nomeAluno, media);
+            }else if( media >= 8 && media <= 10){
+                System.out.printf("Aluno %s APROVADO com %.2f.\n", nomeAluno, media);
             }
 
+            /* atribui media e nome se atender aos requisitos */
+            if(media > mediaMelhorAluno){
+                mediaMelhorAluno = media;
+                nomeMelhorAluno = nomeAluno;
+            }else if (media < mediaPiorAluno){
+                mediaPiorAluno = media;
+                nomePiorAluno = nomeAluno;
+            }
         }
 
+        //Imprime melhor e pior aluno com susas notas
+        System.out.println(nomeMelhorAluno + mediaMelhorAluno);    
+        System.out.println(nomePiorAluno + mediaPiorAluno);
     }
-}
+}        

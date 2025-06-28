85cff43672985eab56ca1ab7492269c3eafee059
Gabriel Avila <gabriel.bravin99@gmail.com>
2025-06-27 22:18:17 -0300

    Criacao arquivos

---
+++ TabelasNotas/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] alunos = new String[3];
+        Double[][] notas = new Double[3][3];
+
+
+
+
+
+    }
+}
